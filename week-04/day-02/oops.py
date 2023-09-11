d22b821f4d4287df0b95a130aec4eff74b17aa1f
Akilan <akil.dove@gmail.com>
2023-09-11 17:25:55 +0530

    Update oops.py

--- week-04/day-02/oops.py
+++ week-04/day-02/oops.py
@@ -1,6 +1,10 @@
 """ Oops Demo"""
-
-
+"""
+_name - Encapsulation
+class Developer(Employee) - Inheritence
+details() - Abstraction
+intro() - polymorphism
+"""
 """Employee class"""
 
 
@@ -9,16 +13,33 @@ class Employee:
     """Init method"""
 
     def __init__(self, name, location, salary):
-        self.name = name
+        # _name -> private property
+        self._name = name
         self.location = location
         self.salary = salary
 
+    # getter
+    @property
+    def name(self):
+        #print("Called name getter")
+        return self._name
+
+    # setter
+    @name.setter
+    def name(self, name):
+        #print("Called name setter")
+        self._name = name
+
     # print(class_instance)
+
     def __str__(self) -> str:
         return f"{self.__class__.__name__} -> Name - {self.name}, Location - {self.location}, Salary - {self.salary}"
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__} -> {self.name} - {self.location} - {self.salary}"
+
+    def details(self) -> str:
+        return f"Details -> Name - {self.name}, Location - {self.location}, Salary - {self.salary}"
 
 
 """ Developer class derived from Employee"""
@@ -32,8 +53,12 @@ class Developer(Employee):
 
     # not associated with class or instance(objects)
     @staticmethod
-    def say_hello(name):
+    def hello(name):
         return f"Hello, {name}"
+
+    def say_hello(self):
+        print(self.name)
+        return f"Hello, I am a {self.prog_lang} Developer "
 
 
 """ Manager class derived from Employee"""
@@ -43,14 +68,36 @@ class Manager(Employee):
     def __init__(self, name, location, salary, emps=None):
         super().__init__(name, location, salary)
 
-        if not emps:
+        if emps is not None:
             self.emps = emps
         else:
             self.emps = []
 
+    def say_hello(self):
+        print(self.emps)
+        return f"Hello,I am a Manager. Managing {self.emps} Employees"
+
+
+def intro(obj):
+    print("Printing object")
+    print(obj)
+    return obj.say_hello()
+
 
 emp_1 = Employee("Akilan", "Tenkasi", 60000)
+print(emp_1.name)
+# Called name getter
+# Akilan
 
+emp_1.name = "Inba"
+print("After setting name")
+print(emp_1.name)
+# After setting name
+# Called name getter
+# Inba
+
+# details
+emp_1.details()
 # calls __str__ method defualt
 print(emp_1)
 # Employee -> Name - Akilan, Location - Tenkasi, Salary - 60000
@@ -64,8 +111,12 @@ print(repr(emp_1))
 
 dev_1 = Developer("Saurav", "Bangalore", 70000, "Rust")
 # call static method
-print(Developer.say_hello(dev_1.name))
+print(Developer.hello(dev_1.name))
 # Hello, Saurav
 mgr_1 = Manager("Madhu", "USA", 80000, [emp_1, dev_1])
 print(mgr_1)
 # Manager -> Name - Madhu, Location - USA, Salary - 80000
+
+
+print(intro(dev_1))
+print(intro(mgr_1))
