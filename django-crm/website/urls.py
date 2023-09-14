c3a810a80dfaff70a91da4d25c851333089e3d77
akilan <akil.dove@gmail.com>
2023-09-14 05:48:14 +0530

    added user registration dcrm

--- django-crm/website/urls.py
+++ django-crm/website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
 ]
--- django-crm/website/views.py
+++ django-crm/website/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
+# Home/Login page
 def home(request):
 
     if request.method == "POST":
@@ -21,7 +22,33 @@ def home(request):
     else:
         return render(request,"home.html",{})
 
+# logout page
 def logout_user(request):
     logout(request)
     messages.success(request,"Logged out successfully")
     return redirect('home')
+
+# Register User
+def register_user(request):
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"Logged in successfully")
+                return redirect('home')
+            else:
+                messages.error(request,"Authentication failed")
+                return redirect('home')
+        else:
+            return render(request, 'register.html', {'form':form})
+    else:
+        form = UserRegisterForm()
+        return render(request,"register.html",{'form': form})
